bdfc8bfb357c5c00fd55a9e763cb6c92d6788734
Rupesh Chand <rupeshchandvasantam@gmail.com>
2025-02-02 12:30:54 +0530

    connect redux to react by using provider

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
+import { Provider } from 'react-redux';
+import Store from './Redux/Store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
